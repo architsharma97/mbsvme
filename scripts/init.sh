d8f6e5697ad21819cc37008ae574bfa474e5e32e
architsharma97 <archit97sharma@gmail.com>
2018-01-09 17:52:13 +0530

    fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,4 +3,4 @@
 mkdir ../data/
 cd ../data
 wget https://github.com/tdiethe/gunnar_raetsch_benchmark_datasets/raw/master/benchmarks_v6.mat
-wget https://github.com/tdiethe/gunnar_raetsch_benchmark_datasets/blob/master/benchmarks.mat
+wget https://github.com/tdiethe/gunnar_raetsch_benchmark_datasets/raw/master/benchmarks.mat
