557fa7aea8ce4477d2ac60d458d709ac481d4277
archit97sharma <archit97sharma@gmail.com>
2018-01-31 04:57:49 -0500

    master file

--- scripts/master_gg.sh
+++ scripts/master_gg.sh
@@ -2,14 +2,14 @@
 for dataset in ijcnn
 do
 	echo "running on "$dataset
-	for num in 20 10 50
+	for num in 15
 	do	
-		for reg_exp in 1.0 2.0 10.0
+		for reg_exp in 1.0 2.0 10.0 5.0 20.0
 		do
 			echo $num" expert(s) in use with regularization at "$reg_exp
-			for rep in {1..1}
+			for rep in {1..2}
 			do 
-				python mbsvme_gen.py -f True -d $dataset -k $num -r $reg_exp --max_iters 70
+				python mbsvme_gen.py -f True -d $dataset -k $num -r $reg_exp --max_iters 60
 			done
 		done
 	done
