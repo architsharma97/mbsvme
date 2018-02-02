16b3ce48f7e5e401966e01c45da001653de36d21
archit97sharma <archit97sharma@gmail.com>
2018-02-02 09:30:55 -0500

    master scripts updated

--- scripts/master_gg.sh
+++ scripts/master_gg.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-for dataset in ijcnn
+for dataset in adult
 do
 	echo "running on "$dataset
-	for num in 25
-	do	
-		for reg_exp in 1.0 2.0 10.0 5.0 20.0
+	for num in 10 1 5 20
+	do
+		for reg_exp in 10.0 1.0 5.0 20.0
 		do
 			echo $num" expert(s) in use with regularization at "$reg_exp
 			for rep in {1..2}
-			do 
-				python mbsvme_gen.py -f True -d $dataset -k $num -r $reg_exp --max_iters 60
+			do
+				python mbsvme_gen.py -p none -f True -d $dataset -k $num -r $reg_exp --max_iters 70
 			done
 		done
 	done
--- scripts/master_pg.sh
+++ scripts/master_pg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for dataset in image
+for dataset in adult
 do
 	echo "running on "$dataset
 	for num in 10 5 1 20 5
@@ -9,9 +9,9 @@ do
 			for reg_gat in 10.0 1.0 5.0
 			do
 				echo $num" expert(s) in use with regularization values ("$reg_exp"," $reg_gat")"
-				for rep in {1..50}
+				for rep in {1..2}
 				do
-					python mbsvme_pg.py -f True -d $dataset -k $num -re $reg_exp -rg $reg_gat --max_iter 60
+					python mbsvme_pg.py -p none -f True -d $dataset -k $num -re $reg_exp -rg $reg_gat --max_iter 70
 				done
 			done
 		done
