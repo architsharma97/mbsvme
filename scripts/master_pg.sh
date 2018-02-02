f5e7a3465491d32f633e42bd313c624ec102b4ae
archit97sharma <archit97sharma@gmail.com>
2018-02-02 05:58:50 -0500

    master script changes

--- scripts/master_pg.sh
+++ scripts/master_pg.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-for dataset in banana waveform breast_cancer image
+for dataset in image
 do
 	echo "running on "$dataset
-	for num in 1 2 5 10 15 20
-	do	
-		for reg_exp in 1.0 2.0 5.0 10.0
+	for num in 10 5 1 20 5
+	do
+		for reg_exp in 1.0 10.0 5.0
 		do
-			for reg_gat in 1.0 2.0 5.0 10.0
+			for reg_gat in 10.0 1.0 5.0
 			do
 				echo $num" expert(s) in use with regularization values ("$reg_exp"," $reg_gat")"
 				for rep in {1..50}
-				do 
+				do
 					python mbsvme_pg.py -f True -d $dataset -k $num -re $reg_exp -rg $reg_gat --max_iter 60
 				done
 			done
