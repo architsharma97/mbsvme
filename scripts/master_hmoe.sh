657709abe1af47dea155c81f4544cc5b6db038a1
archit97sharma <archit97sharma@gmail.com>
2018-05-16 04:03:30 -0400

    commit before committing

--- scripts/master_hmoe.sh
+++ scripts/master_hmoe.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-for dataset in banana
+for dataset in adult
 do
 	echo "running on "$dataset
-	for num in 2 3 4 5 6
+	for num in 6 3
 	do
-		for reg_exp in 10.0 1.0 5.0 20.0
+		for reg_exp in 1.0 5.0 10.0
 		do
 			echo $num" expert(s) in use with regularization at "$reg_exp
 			for taskid in {0..0}
 			do
-				for rep in {1..50}
+				for rep in {1..2}
 				do
 					python hmoe.py -f True -d $dataset -l $num -r $reg_exp --max_iters 60 -p gauss -t $taskid
 				done
